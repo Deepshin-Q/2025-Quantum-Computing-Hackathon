b91fd81ee8edbc1e4c8da5751e6e3f890da3d8d0
Dohyun Kim <dohyun06b@gmail.com>
2025-06-29 16:54:14 +0900

    draw 3-dimension graph

--- 2.py
+++ 2.py
@@ -16,68 +16,75 @@ from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifi
 from qiskit_machine_learning.neural_networks import EffectiveDimension, LocalEffectiveDimension
 from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
 
+from mpl_toolkits.mplot3d import Axes3D
+
 # set random seed
 algorithm_globals.random_seed = 42
 sampler = Sampler()
 estimator = Estimator()
 
-num_qubits = 3
-# combine a custom feature map and ansatz into a single circuit
-qc = QNNCircuit(
-    feature_map=ZFeatureMap(feature_dimension=num_qubits, reps=1),
-    ansatz=RealAmplitudes(num_qubits, reps=1),
-)
-print(qc.draw(output="text", style="clifford"))
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
 
 # parity maps bitstrings to 0 or 1
 def parity(x):
     return "{:b}".format(x).count("1") % 2
 
+plotsX = []
+plotsY = []
+plotsZ = []
 
-output_shape = 2  # corresponds to the number of classes, possible outcomes of the (parity) mapping.
+for num_qubits in range(3,10):
+    for depth in range(1, 10):
+        # combine a custom feature map and ansatz into a single circuit
+        qc = QNNCircuit(
+            feature_map=ZFeatureMap(feature_dimension=num_qubits, reps=depth),
+            ansatz=RealAmplitudes(num_qubits, reps=depth),
+        )
+        
+        output_shape = 2  # corresponds to the number of classes, possible outcomes of the (parity) mapping.
 
-# construct QNN
-qnn = SamplerQNN(
-    circuit=qc,
-    interpret=parity,
-    output_shape=output_shape,
-    sparse=False,
-    sampler=sampler,
-)
+        # construct QNN
+        qnn = SamplerQNN(
+            circuit=qc,
+            interpret=parity,
+            output_shape=output_shape,
+            sparse=False,
+            sampler=sampler,
+        )
 
-# we can set the total number of input samples and weight samples for random selection
-num_input_samples = 10
-num_weight_samples = 10
+        # we can set the total number of input samples and weight samples for random selection
+        num_input_samples = 10
+        num_weight_samples = 10
 
-global_ed = EffectiveDimension(
-    qnn=qnn, weight_samples=num_weight_samples, input_samples=num_input_samples
-)
+        global_ed = EffectiveDimension(
+            qnn=qnn, weight_samples=num_weight_samples, input_samples=num_input_samples
+        )
 
-# we can also provide user-defined samples and parameters
-input_samples = algorithm_globals.random.normal(0, 1, size=(10, qnn.num_inputs))
-weight_samples = algorithm_globals.random.uniform(0, 1, size=(10, qnn.num_weights))
+        # we can also provide user-defined samples and parameters
+        input_samples = algorithm_globals.random.normal(0, 1, size=(10, qnn.num_inputs))
+        weight_samples = algorithm_globals.random.uniform(0, 1, size=(10, qnn.num_weights))
 
-global_ed = EffectiveDimension(qnn=qnn, weight_samples=weight_samples, input_samples=input_samples)
+        global_ed = EffectiveDimension(qnn=qnn, weight_samples=weight_samples, input_samples=input_samples)
 
-# finally, we will define ranges to test different numbers of data, n
-n = [5000, 8000, 10000, 40000, 60000, 100000, 150000, 200000, 500000, 1000000]
+        # finally, we will define ranges to test different numbers of data, n
+        n = [5000, 8000, 10000, 40000, 60000, 100000, 150000, 200000, 500000, 1000000]
 
-global_eff_dim_0 = global_ed.get_effective_dimension(dataset_size=n[0])
+        global_eff_dim_1 = global_ed.get_effective_dimension(dataset_size=n[6])
 
-d = qnn.num_weights
+        d = qnn.num_weights    
 
-print("Data size: {}, global effective dimension: {:.4f}".format(n[0], global_eff_dim_0))
-print(
-    "Number of weights: {}, normalized effective dimension: {:.4f}".format(d, global_eff_dim_0 / d)
-)
+        print("Effective dimension: {}".format(global_eff_dim_1))
+        print("Number of weights: {}".format(d))
 
-global_eff_dim_1 = global_ed.get_effective_dimension(dataset_size=n)
-
-print("Effective dimension: {}".format(global_eff_dim_1))
-print("Number of weights: {}".format(d))
+        plotsX.append(num_qubits)
+        plotsY.append(global_eff_dim_1 / d)
+        plotsZ.append(depth)
 
 # plot the normalized effective dimension for the model
-plt.plot(n, np.array(global_eff_dim_1) / d)
-plt.xlabel("Number of data")
-plt.ylabel("Normalized GLOBAL effective dimension")
+ax.scatter(plotsX, plotsY, plotsZ, c=plotsY, cmap='viridis', label='Model Points')
+ax.set_xlabel("Number of qubits")
+ax.set_ylabel("Normalized GLOBAL effective dimension")
+ax.set_zlabel("Depth of the circuit")
+ax.legend()
 plt.show()
